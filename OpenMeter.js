876f5c53e26a3d8be1a009fbbd184645bdc92d9d
Thorsten Zoerner <me@thorsten-zoerner.com>
2021-12-24 00:23:04 +0100

    v0.1.1

---
+++ OpenMeter.js
@@ -0,0 +1,37 @@
+module.exports = function(RED) {
+    function OpenMeterNode(config) {
+        const axios = require("axios");
+        RED.nodes.createNode(this,config);
+        const node = this;
+        const storage = node.context();
+        node.on('input', async function(msg) {
+            // check source of meterId
+            let meterId = null;
+            if ((typeof msg.payload === 'string' || msg.payload instanceof String)&&(msg.payload.length > 30)) {
+              meterId = msg.payload;
+              console.log('h1');
+            } else
+            if (typeof msg.payload.meterId !== 'undefined') {
+              meterId = msg.payload.meterId;
+              console.log('h2');
+            } else
+            if((typeof config.meterId !== 'undefined') && (config.meterId !== null) && (config.meterId.length > 30)) {
+              meterId = config.meterId;
+              console.log('h3');
+            } else {
+              meterId = await storage.get("meterId");
+              console.log('in Here',meterId);
+            }
+            console.log('out Here',meterId);
+            if((typeof meterId == 'undefined') || (meterId == null)  || (meterId.length < 10)) {
+              const availMetersResponds = await axios.get("https://openmeter.discovergy.com/public/v1/meters");
+              const availMeters = availMetersResponds.data;
+              meterId = availMeters[Math.floor(availMeters.length * Math.random())].meterId;
+            }
+            await storage.set("meterId",meterId);
+            let lastReading = await axios.get("https://openmeter.discovergy.com/public/v1/last_reading?meterId="+meterId);
+            node.send({payload:lastReading.data}  );
+        });
+    }
+    RED.nodes.registerType("OpenMeter",OpenMeterNode);
+}
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # node-red-contrib-openmeter
-Access to Openmeter measurements (last reading) of OpenMeter Project (Germany)
+
+<a href="https://stromdao.de/" target="_blank" title="STROMDAO - Digital Energy Infrastructure"><img src="./static/stromdao.png" align="right" height="85px" hspace="30px" vspace="30px"></a>
+
+**Access to Openmeter measurements (last reading) of OpenMeter Project (Germany)**
+
+Implementation of the OpenMeter access API available at https://openmeter.discovergy.com/ for Node RED.
+
+[![npm](https://img.shields.io/npm/dt/node-red-contrib-openmeter.svg)](https://www.npmjs.com/package/node-red-contrib-openmeter)
+[![npm](https://img.shields.io/npm/v/node-red-contrib-openmeter.svg)](https://www.npmjs.com/package/node-red-contrib-openmeter)
+[![CO2Offset](https://api.corrently.io/v2.0/ghgmanage/statusimg?host=node-red-contrib-openmeter&svg=1)](https://co2offset.io/badge.html?host=node-red-contrib-openmeter)
+
+## Configuration
+
+Install using Node-RED Package Manager (Palett)
+
+## Usage
+
+Specify meterId either in Node Configuration or in `msg.payload`. 
+
+## Maintainer / Imprint
+
+<addr>
+STROMDAO GmbH  <br/>
+Gerhard Weiser Ring 29  <br/>
+69256 Mauer  <br/>
+Germany  <br/>
+  <br/>
++49 6226 968 009 0  <br/>
+  <br/>
+kontakt@stromdao.com  <br/>
+  <br/>
+Handelsregister: HRB 728691 (Amtsgericht Mannheim)
+</addr>
+
+Project Website: https://co2offset.io/
+
+## LICENSE
+[Apache-2.0](./LICENSE)
